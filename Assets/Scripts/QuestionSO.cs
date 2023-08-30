1b17eae7f156f69f3183d87489c007fba10c09b8
youngil <youngil@placeb.com>
2023-08-30 21:57:20 +0900

    Scriptable Object

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2, 6)]
+    [SerializeField] string question = "새로운 문제를 여기에 입력하세요";
+
+}
