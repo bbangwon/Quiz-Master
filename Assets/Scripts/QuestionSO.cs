2983a9452fa89535bee84030429f6050d1e69d46
youngil <youngil@placeb.com>
2023-08-31 08:56:05 +0900

    Getter Method

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -6,4 +6,9 @@ public class QuestionSO : ScriptableObject
     [TextArea(2, 6)]
     [SerializeField] string question = "새로운 문제를 여기에 입력하세요";
 
+    public string GetQuestion()
+    {
+        return question;
+    }
+
 }
