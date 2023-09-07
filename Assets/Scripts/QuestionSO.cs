26046fa4df192a0342cdb8c7f4872fc51e49b6e4
youngil <youngil@placeb.com>
2023-09-07 09:24:56 +0900

    Array

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -5,10 +5,20 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] string question = "새로운 문제를 여기에 입력하세요";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex = 0;
 
     public string GetQuestion()
     {
         return question;
     }
+    public string GetAnswer(int index)
+    {
+        return answers[index];        
+    }
 
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
 }
